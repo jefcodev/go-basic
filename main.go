bc35fb81ebe3be1e953ee1c6cb5e488a36f3d6a7
jefcodev <jeffersonpinchao@hotmail.com>
2023-01-10 18:51:37 -0500

    Tarea #1 CRUD sencillo

--- main.go
+++ main.go
@@ -11,111 +11,94 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<html> <body> Hola <br> Mundo</body></html>")
+	fmt.Fprintf(w, "<html> <body> Proformas</body></html>")
 }
 
 func main() {
-	Productos = []Producto{
-		Producto{Id: "1", Nombre: "Monitor 17 pulgadas", Descripcion: "Conexión HDMI - Full HD", Cantidad: 3},
-		Producto{Id: "2", Nombre: "Teclado USB", Descripcion: "Color negro y teclas multimedia", Cantidad: 7},
+	Proformas = []Proforma{
+		Proforma{Id: "1", NumProforma: "PRO-001-001-00000191", Cliente: "Jose Perez", Cantidad: 3, Precio: 158.10, Total: 473.30},
+		Proforma{Id: "2", NumProforma: "PRO-001-001-00000192", Cliente: "Maria Alveaer", Cantidad: 2, Precio: 100.10, Total: 300.00},
 	}
 	iniciarServidor()
 }
 
-// Estructura del la tabla producto
+// Estructura del la tabla prformas
 
-type Producto struct {
-	Id          string `json:id`
-	Nombre      string `json:nombre`
-	Descripcion string `json:descripcion`
-	Cantidad    int    `json:cantidad`
+type Proforma struct {
+	Id          string  `json:id`
+	NumProforma string  `json:numProforma`
+	Cliente     string  `json:cliente`
+	Cantidad    int     `json:cantidad`
+	Precio      float32 `json:precio`
+	Total       float32 `json:total`
 }
 
-// Array global de productos:
-var Productos []Producto
+// Array global de proforma:
+var Proformas []Proforma
 
-// Metodo para encontrar todos los productos
+// Metodo para encontrar todos los proformas
 
-func findAllProductos(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Solicitud atendida: findAllProductos")
-	json.NewEncoder(w).Encode(Productos)
+func findAllProformas(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Solicitud atendida: findAllProformas")
+	json.NewEncoder(w).Encode(Proformas)
 }
 
-func createNewProducto(w http.ResponseWriter, r *http.Request) {
+func createNewProforma(w http.ResponseWriter, r *http.Request) {
 	// Se obtiene el body desde el request y
-	// se deserializa en una variable producto:
+	// se deserializa en una variable proforma:
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	var producto Producto
-	json.Unmarshal(reqBody, &producto)
-	// adicionamos en el array el nuevo producto:
-	Productos = append(Productos, producto)
-	json.NewEncoder(w).Encode(producto)
+	var proforma Proforma
+	json.Unmarshal(reqBody, &proforma)
+	// adicionamos en el array la nueva proforma:
+	Proformas = append(Proformas, proforma)
+	json.NewEncoder(w).Encode(proforma)
 }
 
-// Función get para consultar producto por id
+// Función borrar un proformas mediante su id
 
-func findProductoById(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Solicitud atendida: findProductoById")
+func deleteProforma(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Solicitud atendida: deleteProforma")
 	vars := mux.Vars(r)
 	key := vars["id"]
-	for _, producto := range Productos {
-		if producto.Id == key {
-			json.NewEncoder(w).Encode(producto)
-		}
-	}
-}
-
-// Función borrar un producto mediante su id
-
-func deleteProducto(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Solicitud atendida: deleteProducto")
-	vars := mux.Vars(r)
-	key := vars["id"]
-	// buscar el producto a eliminar:
-	for index, producto := range Productos {
-		if producto.Id == key {
+	// buscar la proforma a eliminar:
+	for index, proforma := range Proformas {
+		if proforma.Id == key {
 			// borrar del array:
-			Productos = append(Productos[:index], Productos[index+1:]...)
+			Proformas = append(Proformas[:index], Proformas[index+1:]...)
 		}
 	}
 }
 
-// Actualizar los datos de un producto
+// Actualizar los datos de una proforma
 
-func updateProducto(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Solicitud atendida: updateProducto")
+func updateProforma(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Solicitud atendida: updateProforma")
 	// Se obtiene el body desde el request y
-	// se deserializa en una variable producto:
+	// se deserializa en una variable proforma:
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	var producto Producto
-	json.Unmarshal(reqBody, &producto)
-	key := producto.Id
-	// buscar el producto a actualizar:
-	for index, p := range Productos {
+	var proforma Proforma
+	json.Unmarshal(reqBody, &proforma)
+	key := proforma.Id
+	// buscar la proforma a actualizar:
+	for index, p := range Proformas {
 		if p.Id == key {
 			// actualizar el array:
-			Productos[index] = producto
+			Proformas[index] = proforma
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(producto)
+	json.NewEncoder(w).Encode(proforma)
 }
 
 // Se agregan las rutas y se inicia el servidor
 
 func iniciarServidor() {
-	/* fmt.Println("API REST simple con lenguaje go.")
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/productos", findAllProductos)
-	log.Fatal(http.ListenAndServe(":8080", nil)) */
-
 	fmt.Println("API REST simple con lenguaje go.")
 	ruteador := mux.NewRouter().StrictSlash(true)
 	ruteador.HandleFunc("/", homePage)
-	ruteador.HandleFunc("/productos", findAllProductos)
-	ruteador.HandleFunc("/producto", createNewProducto).Methods("POST")
-	ruteador.HandleFunc("/producto", updateProducto).Methods("PUT")
-	ruteador.HandleFunc("/producto/{id}", deleteProducto).Methods("DELETE")
-	ruteador.HandleFunc("/producto/{id}", findProductoById)
+	ruteador.HandleFunc("/proformas", findAllProformas)
+	ruteador.HandleFunc("/proforma", createNewProforma).Methods("POST")
+	ruteador.HandleFunc("/proforma", updateProforma).Methods("PUT")
+	ruteador.HandleFunc("/proforma/{id}", deleteProforma).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", ruteador))
 }
